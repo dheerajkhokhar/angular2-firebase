a36abeafd84416270b8ddae2e65bc108e0194263
Dheeraj Kumar <dheeraj.kumar@io-media.com>
2016-12-13 18:06:02 +0530

    firebase initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  cuisines : FirebaseListObservable<any[]>;
+  restaurant;
+
+  constructor(private _af:AngularFire){}
+
+  ngOnInit(){
+  //  this.subscription = this._af.database.list('/cuisines').subscribe((x) => {
+  //     this.cuisines = x;
+  //     console.log(this.cuisines);
+  //   })
+
+    this.cuisines = this._af.database.list('/cuisines');
+    this.restaurant = this._af.database.object('/restaurant');
+  }
+
+  add() {
+    this.cuisines.push('Asian');
+  }
+
+  update(){
+    this._af.database.object('/restaurant').set({
+      name: 'New Name',
+      rating: 5
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+  export const firebaseConfig = {
+    apiKey: "AIzaSyCbcMSZ3SkBs4z3CCafEpl6QMdf4-LKS9w",
+    authDomain: "pepper-808dc.firebaseapp.com",
+    databaseURL: "https://pepper-808dc.firebaseio.com",
+    storageBucket: "pepper-808dc.appspot.com",
+    messagingSenderId: "1063648750448"
+  }
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
